9bf3eade0284afbf3fd4f6ab18dc429112beb206
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-01-28 10:01:46 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter, Routes, Route, Navigate} from 'react-router';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route>
+          <Route path="/" element={}/>
+          <Route path="*" element={<Navigate replace to="/"/>} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
