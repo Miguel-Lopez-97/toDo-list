00b7745d9745f36c61abeb171462996bc5ffcd3b
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-01-28 10:20:02 -0500

    Route Home in Index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Routes, Route, Navigate} from 'react-router';
+import {HashRouter, Routes, Route, Navigate} from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import {Home} from './Components/Home/Home'
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
         <Route>
-          <Route path="/" element={}/>
+          <Route path="/" element={<Home/>}/>
           <Route path="*" element={<Navigate replace to="/"/>} />
         </Route>
       </Routes>
